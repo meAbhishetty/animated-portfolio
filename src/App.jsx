5d9016f76473c563952e1a9fa2409363f07b9477
Abhishek Shetty <abhishekshetty817@gmail.com>
2023-10-27 21:06:54 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.scss";
+import Navbar from "./components/Navbar";
+const App = () => {
+    return (
+        <div>
+            <section>
+                <Navbar />
+            </section>
+            <section>parallax</section>
+            <section>services</section>
+            <section>parallax</section>
+            <section>port 1</section>
+            <section>port 2</section>
+            <section>port 3</section>
+            <section>contact</section>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+    return <div className="navbar">
+        {/* sidebar */}
+        <div className="navbar-wrapper">
+            <span>Abhishek</span>
+            <div className="social">
+                
+            </div>
+        </div>
+    </div>;
+};
+
+export default Navbar;
